d89e78c213f2e88699df10e5a4b8c57757454aaf
Giulio Starace <giulio.starace@gmail.com>
2019-11-18 00:54:46 -0300

    define evaluation logic

--- GPTrueOrFalse/src/content/content.js
+++ GPTrueOrFalse/src/content/content.js
@@ -1,5 +1,11 @@
-browser.runtime.onMessage.addListener(gotMessage);
+// clear evaluations when user clicks away
+$("html").on("blur focus keydown mousedown", function() {
+  $(".GPT2Message").remove();
+  $(".afterMeGpt2").removeClass("afterMeGpt2");
+});
 
+// communicate with popup
+browser.runtime.onMessage.addListener(gotMessage);
 function gotMessage(message) {
   if (message.type === "selection-check") {
     return checkSelection();
@@ -8,14 +14,44 @@ function gotMessage(message) {
   }
 }
 
+// determines how real a portion of selected text is
 function evaluateSelection() {
-  console.log("TODO");
-  // window.getSelection()
-
-  // Selection {anchorNode: text, anchorOffset: 139, focusNode: text, focusOffset: 0, isCollapsed: false, …}
-  // window.getSelection()
-
-  // Selection {anchorNode: text, anchorOffset: 0, focusNode: text, focusOffset: 139, isCollapsed: false, …}
+  // initiate element variable
+  let element;
+  // get the selected text
+  let selection = window.getSelection();
+  // determine which node is at the bottom of the selection and assign it to the element
+  if (selection.anchorOffset && !selection.focusOffset) {
+    element = selection.anchorNode.parentNode;
+  } else if (selection.focusOffset && !selection.anchorOffset) {
+    element = selection.focusNode.parentNode;
+  }
+  // parse the selected text
+  let text = selection.toString();
+  // evaluate
+  fetch(
+    "https://huggingface.co/openai-detector/?" +
+      encodeURI(text.replace(/[\r\n]+/g, ""))
+  )
+    .then(response => response.json())
+    .then(json => {
+      // parse the response from Hugging Face
+      let all_tokens = json.all_tokens;
+      let used_tokens = json.used_tokens;
+      let realness = Math.round(json.real_probability * 100 * 100) / 100;
+      let css_color_string = generateHSLString(realness, 0, 120);
+      // mark the element after which to insert the result of the evaluation
+      element.className += " " + "afterMeGpt2";
+      // prepare the message
+      let message_text = `According to the detector, there is a ${realness} % chance that the selected text is real. ${used_tokens} out of ${all_tokens} tokens were considered`;
+      // prepare the message element
+      let message_element = $(`<p class='GPT2Message'>${message_text}</p>`);
+      message_element.css("border", `2px dashed ${css_color_string}`);
+      message_element.css("border-top", `5px solid ${css_color_string}`);
+      // insert the message element
+      message_element.insertAfter(".afterMeGpt2");
+    })
+    .catch(err => console.log(err));
 }
 
 // checks whether the selected text is satisfactory for our needs and sends response
@@ -42,4 +78,14 @@ function getSelectedText() {
     text = window.getSelection();
   }
   return text;
+}
+
+// generates HSL string given on a percentage between a start and end color
+function generateHSLString(percent, start, end) {
+  let fraction = percent / 100;
+  let offset = (end - start) * fraction;
+  let hue = offset + start;
+
+  // Return a CSS HSL string
+  return `hsl(${hue}, 100%, 50%)`;
 }
